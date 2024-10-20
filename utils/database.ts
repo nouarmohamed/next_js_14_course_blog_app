0a7033b086bdf82fade166364ffe504b324b0870
nouarmohamed <mohamed.mohnouar@gmail.com>
2024-10-20 12:51:51 +0100

    Initial commit

--- utils/database.ts
+++ utils/database.ts
@@ -1,25 +1,23 @@
 import mongoose from "mongoose";
 
-let isConnected = false; // Variable to track the connection status
+let isConnected = false; 
 
 export const connectToDB = async () => {
-  // Set strict query mode for Mongoose to prevent unknown field queries.
+ 
   mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URI) return console.log("Missing MongoDB URL");
 
-  // If the connection is already established, return without creating a new connection.
+  
   if (isConnected) {
     console.log("MongoDB connection already established");
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      connectTimeoutMS: 10000
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
 
-    isConnected = true; // Set the connection status to true
+    isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
