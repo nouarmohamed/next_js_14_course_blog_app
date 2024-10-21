44fed508f7a6327cc8f9a633fb44c9a3b0d24001
nouarmohamed <mohamed.mohnouar@gmail.com>
2024-10-21 11:47:49 +0100

    fix error

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -6,7 +6,13 @@ export const GET = async (req: Request) => {
     try {
         await connectToDB();
         const posts = await Post.find({}).populate('author').sort({'createAt': -1});
-        return NextResponse.json(posts, { status: 200 });
+        return NextResponse.json(posts, { status: 200, 
+            headers: {
+            'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+            'Pragma': 'no-cache',
+            'Expires': '0',
+            'Surrogate-Control': 'no-store'
+        }});
     } catch (error) {
         console.error("Error fetching posts:", error);
         return NextResponse.json({ error: "Failed to fetch posts"}, { status: 500 });
