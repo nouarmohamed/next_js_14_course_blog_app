97297f8af3b1f6c202f4c9945ca68e01b1db134e
nouarmohamed <mohamed.mohnouar@gmail.com>
2024-10-21 13:17:16 +0100

    fix error6

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,13 +3,14 @@ import Post from "@/utils/models/post";
 import { NextResponse } from "next/server";
 
 export const GET = async (req: Request) => {
-    try {
+    try {console.log('no thing')
         await connectToDB();
         console.log('kaddooor')
-        const posts = await Post.find({}).populate('author').sort({'createAt': -1});
+        const posts = await Post.find({}).populate('author');
         console.log('mohemad', posts)
         return NextResponse.json(posts, { status: 200 });
     } catch (error) {
+        console.log('error kbiiir')
         return NextResponse.json({ error: "Failed to fetch posts"}, { status: 500 });
     }
 };
